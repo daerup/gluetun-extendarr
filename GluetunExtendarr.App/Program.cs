bc71bdb5502d02e7619492a23537ed5e7e1d8b47
Dario Portmann <daerup@gmx.ch>
2025-01-03 02:08:43 +0100

    improved docker iamge

--- GluetunExtendarr.App/Program.cs
+++ GluetunExtendarr.App/Program.cs
@@ -1,17 +1,14 @@
 using GluetunExtendarr.App;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
 
+builder.Logging.AddConsole();
 builder.Configuration.AddJsonFile("appsettings.json").AddEnvironmentVariables();
 builder.Services.Configure<Settings>(builder.Configuration);
 builder.Services.AddHealthChecks().AddCheck("Docker Check", Endpoints.HealthCheckAction);
-
 var app = builder.Build();
 
 app.MapPost("/run", Endpoints.RunAction);
 app.MapHealthChecks("/healthz");
-
-app.UseHttpsRedirection();
-
 
 app.Run();
