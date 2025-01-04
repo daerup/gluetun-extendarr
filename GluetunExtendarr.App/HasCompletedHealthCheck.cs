13c5d8033abb317f9d4dfc231dc86bf7d267e502
Dario Portmann <daerup@gmx.ch>
2025-01-04 01:21:06 +0100

    improved HasCompletedHealthCheck

--- GluetunExtendarr.App/HasCompletedHealthCheck.cs
+++ GluetunExtendarr.App/HasCompletedHealthCheck.cs
@@ -5,7 +5,7 @@ namespace GluetunExtendarr.App;
 public class HasCompletedHealthCheck(ICompletable completable) : IHealthCheck
 {
     private readonly HealthCheckResult healthy = HealthCheckResult.Healthy("Config file has been created");
-    private readonly HealthCheckResult unhealthy = HealthCheckResult.Unhealthy("GluetunExtendarr has not run yet");
+    private readonly HealthCheckResult unhealthy = HealthCheckResult.Unhealthy($"{nameof(ExtendarrRunner)} has not completed yet");
 
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken()) => Task.FromResult(completable.HasCompleted ? healthy : unhealthy);
 }
